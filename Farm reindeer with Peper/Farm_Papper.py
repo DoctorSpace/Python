b4f88040676eb836b129d02c2c9d3dfae23fcc46
DoctorSpace <40442366+DoctorSpace@users.noreply.github.com>
2021-04-09 12:47:56 +0300

    Update Farm_Papper.py

--- Farm reindeer with Peper/Farm_Papper.py
+++ Farm reindeer with Peper/Farm_Papper.py
@@ -6,53 +6,36 @@ from multiprocessing import Pool
 
 def main(login):
     #Добавляем в проект браузер
-    driver = webdriver.Chrome(executable_path="C://Users//Doctor Space//YandexDisk//code//Python//peper_bot//chromedriver.exe")
+    driver = webdriver.Chrome('chromedriver.exe')
 
     driver.get("https://www.pepper.ru/new") 
-    #driver.get("local_html") 
 
     driver.find_element_by_class_name("btn--mode-header-login").click()
+
     sleep(2)
-    
-    driver.find_element_by_id("loginModalForm-identity").send_keys(login)
-    driver.find_element_by_id("loginModalForm-password").send_keys(password)
-    
+
+    login_field = driver.find_element_by_id("loginModalForm-identity").send_keys(login)
+    pass_field = driver.find_element_by_id("loginModalForm-password").send_keys(password)
+
     driver.find_element_by_class_name("cept-login-submit").click()
-    
-    sleep(2)
+    sleep(1)
 
 
     while 1:
-        #Время задежки
-        Time_for_wait = 5
         driver.get("https://www.pepper.ru/new") 
-    
-        for _ in range(60*Time_for_wait):
+        
+        minutes_to_update = 2
+        for seconds in range(60*minutes_to_update):
             try:
-                driver.find_element_by_class_name("ms-btn--primary").click()
-                print(ctime(time()),"mc-btn--primary", login)
+                driver.find_element_by_class_name("mc-btn--primary").click()
+                print(ctime(time()), login, seconds)
                 sleep(5)
+                driver.get_screenshot_as_file(f"media\{time()} {login}.png")
                 break
             except:
-                sleep(1/3)
-    
-            try:
-                driver.find_element_by_class_name("width--fromW3-7").click()
-                print(ctime(time()),"width--fromW3-7", login)
-                sleep(5)
-                break
-            except:
-                sleep(1/3)
-    
-    
-            try:
-                driver.find_element_by_class_name("mc-notification-inner").click()
-                print(ctime(time()),"mc-notification-inner", login)
-                sleep(5)
-                break
-            except:
-                sleep(1/3)
-    
+                sleep(1)
+
+   
 if __name__ == "__main__":
     logins = [login_1, login_2, login_3]
     p = Pool(processes = len(logins))
