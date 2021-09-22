e1c9d0a27e2ba5d4fecf950de3d32e43464c9889
DoctorSpace <40442366+DoctorSpace@users.noreply.github.com>
2021-09-22 23:59:17 +0300

    Update Bloker.py

--- Block mouse and keyboard/Bloker.py
+++ Block mouse and keyboard/Bloker.py
@@ -4,7 +4,7 @@ from ctypes import *
 import time,os
 
 
-token = "3611868f28474c1632dfe911f66163338ffc190cb124ed41c2efec33f4d5c4a32c45df079d00e36125437"  #token from the VK group
+token = "x"  #token from the VK group
 vk_session = vk_api.VkApi(token = token)
 
 session_api = vk_session.get_api()
@@ -15,12 +15,12 @@ longpoll = VkLongPoll(vk_session)
 while True:
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
-            if str(event.text) == "N":   #when message N comes
+            if str(event.text) == "N":   #Block
 
                 windll.user32.BlockInput(True)
                 print("All_Block")
 
-            if str(event.text) == "Y":   #when message N comes
+            if str(event.text) == "Y":   #UnBlock
              
                 windll.user32.BlockInput(False) 
                 print("All_UnBlock")
