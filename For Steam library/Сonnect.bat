042528e72f1aaafb2b1a9d540f9c60ebfb839b1c
DoctorSpace <40442366+DoctorSpace@users.noreply.github.com>
2022-07-12 15:33:53 +0300

    Update Сonnect.bat

--- For Steam library/Сonnect.bat
+++ For Steam library/Сonnect.bat
@@ -1,3 +1,8 @@
 echo off
 title Create exe
 pyinstaller --onefile --icon="C:\Users\Doctor Space\YandexDisk\code\Python\Steam_Game\Game.ico" "C:\Users\Doctor Space\YandexDisk\code\Python\Steam_Game\Steam_Game.py"
+
+or
+echo off
+pyinstaller -F -i "C:\Users\Doctor Space\YandexDisk\code\Python\Steam_Game\Game.ico" Steam_Game.py
+
