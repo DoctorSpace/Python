da4f79ba5407514fab390812d20ca3d5b5ab1bad
DoctorSpace <40442366+DoctorSpace@users.noreply.github.com>
2023-03-30 05:01:06 +0300

    ScreenshotDualsense v0.1
    A program for creating screenshots using DualSense, without third-party software. Through the "Share" button

---
+++ ScreenshotDualsense/README.md
@@ -0,0 +1,16 @@
+# Screenshot Dualsense with saving Adaptive Triggers
+
+How to use:
+
+1. Using python 3.10.9 <a  href="https://www.python.org/downloads/release/python-3109/"> Download </a>
+2. Install Python (example "C:\Programs\Python")
+3. Open cmd, and enter ```cd C:\Programs\Python\Scripts``` then ```pip install --upgrade pip```, dont close cmd
+4. Add pygame ```pip install pygame```
+5. Connect Dualsense
+6. Run "Start.bat"
+7. Click "Share" to take a screenshot in DualSense
+8. Screenshots are saved to the Screenshot folder Dualsense\ScreenShots
+
+With the <a  href="https://github.com/DoctorSpace/Python/tree/main/ImageToSteam"> ImageToSteam </a> script, you can quickly upload the resulting screenshots to Steam
+
+[<img alt='Here is the button Shere' target="_blank" src="https://github.com/DoctorSpace/DoctorSpace/blob/main/ScreenshotDualsense/Share.png"/>]
---
+++ ScreenshotDualsense/ScreenshotDualsense.py
@@ -0,0 +1,101 @@
+import pygame
+from datetime import datetime
+import time
+import sys
+import win32gui
+import win32ui
+import win32con
+
+# ScreenshotDualsense v0.1
+
+
+def main():
+    pygame.init()
+    # Create a flag to understand if the controller is connected
+    controller_connected = True
+
+    # Check for a connected gamepad
+    def checkGamepad():
+        try:
+            joystick = pygame.joystick.Joystick(0)
+            time.sleep(1)
+            pygame.joystick.init()
+            joystick.init()
+            print('DualSense connected')
+
+        except:
+            print("Checking...")
+            time.sleep(5)
+            checkGamepad()
+
+    checkGamepad()
+    joystick = pygame.joystick.Joystick(0)
+
+    while True:
+        # Handle Pygame events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Check if the controller is connected
+        if not pygame.joystick.get_count():
+            if controller_connected:
+                print("DualSense disabled")
+                controller_connected = False
+                main()
+            continue
+        else:
+            if not controller_connected:
+                print("DualSense connected!")
+                controller_connected = True
+
+        # Customizing the Screenshot Button
+        if controller_connected:
+            if joystick.get_button(4): # 4 = Share | 5 = PS | 6 = Option | 7 = L3 | 8 = R3 | 9 = L2 | 10 = R2
+
+                # Clicking on the gamepad
+                print("screen")
+                now = datetime.now() 
+                start = now.strftime("%S") 
+
+                # Get a device context (DC) handle for the entire screen
+                hdesktop = win32gui.GetDesktopWindow()
+                desktop_dc = win32gui.GetWindowDC(hdesktop)
+                dc = win32ui.CreateDCFromHandle(desktop_dc)
+
+                # Get screen dimensions
+                width = dc.GetDeviceCaps(win32con.HORZRES)
+                height = dc.GetDeviceCaps(win32con.VERTRES)
+
+                # Create a bitmap to save the image
+                bitmap = win32ui.CreateBitmap()
+                bitmap.CreateCompatibleBitmap(dc, width, height)
+                dc_with_bitmap = dc.CreateCompatibleDC()
+
+                # Copy the image to the bitmap
+                dc_with_bitmap.SelectObject(bitmap)
+                dc_with_bitmap.BitBlt((0, 0), (width, height), dc, (0, 0), win32con.SRCCOPY)
+
+
+                # Save image to file
+                current_time = now.strftime("%Y%B%d %H-%M-%S-%f")
+                bitmap.SaveBitmapFile(dc_with_bitmap, f"ScreenShots/{current_time}.png")
+
+                # Free up resources
+                dc.DeleteDC()
+                dc_with_bitmap.DeleteDC()
+                win32gui.ReleaseDC(hdesktop, desktop_dc)
+                win32gui.DeleteObject(bitmap.GetHandle())
+
+                # Measure Screenshot Delay
+                now = datetime.now() 
+                stop = now.strftime("%S") 
+                print("Delay: ",abs(int(start) - int(stop)), " sec")
+    
+
+        pygame.time.wait(3)
+
+
+if __name__== "__main__":
+    main()
